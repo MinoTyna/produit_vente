9358bd95985680b495be7b5a29392ec7965eab62
ChristineRazafisoa <christinerasoanambinina@gmail.com>
2025-08-02 13:05:24 +0300

    base de donne en ligne

--- monbackend/settings.py
+++ monbackend/settings.py
@@ -107,27 +107,27 @@ REST_FRAMEWORK = {
 
 # else :
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv("DB_NAME"),
-#             'USER': os.getenv("DB_USER"),
-#             'PASSWORD': os.getenv("DB_PASSWORD"),
-#             'HOST': os.getenv("DB_HOST"), 
-#             'PORT': os.getenv("DB_PORT"),        
-#         }
-#     }
-
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'test',
-            'USER': 'postgres',
-            'PASSWORD': '1234',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"), 
+            'PORT': os.getenv("DB_PORT"),        
         }
     }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'test',
+#             'USER': 'postgres',
+#             'PASSWORD': '1234',
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
